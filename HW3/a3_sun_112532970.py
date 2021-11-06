2714262f9d40702cfc70d2e5b54c90e4b3f2829d
Andrew Sun <adsun701@gmail.com>
2021-11-06 18:06:54 -0400

    remove need for sklearn

--- HW3/a3_sun_112532970.py
+++ HW3/a3_sun_112532970.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 import csv
 import time
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-
 from sklearn.datasets import load_wine
 wines = load_wine()
 
@@ -21,6 +18,12 @@ is_class1 = (wines.target==1).astype(int)
 np.seterr(all='raise')
 
 #print('(is_class1, target)', list(zip(is_class1[:5], #wines.target[:5])))
+
+# standardizes an array.
+def standardize(arr):
+  std = sampleStd(arr)
+  mu = mean(arr)
+  return [(n - mu) / std for n in arr]
 
 # returns the mean of an array.
 def mean(arr):
@@ -136,14 +139,64 @@ def grad_descent(x_data, y_data):
     i0 += 1
   return (b1, b0)
 
+def sigmoid(x, b0, b1):
+  return (np.e ** (b0 + b1 * x)) / (1 + np.e **(b0 + b1 * x))
 
-def gdm_update_yhat_data(b0, b1s, x_datas):
+# gradient descent algorithm for logistic regression,
+# returns the coefficient and y-intercept respectively of
+# the logistic regression equation
+def grad_descent_log(x_data, y_data):
+  prev_b0 = -1
+  prev_b1 = -1
+  b0 = 0
+  b1 = 0
+  alpha = 0.000001
+  timesDivided = 0
+  timesOverflowed = 0
+  prev_rss = np.inf
+  current_rss = np.inf
+  done = False
+  i0 = 0
+  overFlowStep = -1
+  while(done == False):
+    yhat_data = [sigmoid(x, b0, b1) for x in x_data]
+    prev_rss = current_rss
+    current_rss = rss(y_data, yhat_data)
+    if (current_rss > prev_rss):
+      timesOverflowed += 1
+      overFlowStep = i0
+      alpha /= 10
+      if (timesOverflowed < 3):
+        timesDivided += 1
+        timesOverflowed = 0
+      b0 = prev_b0
+      b1 = prev_b1
+      continue
+    if (prev_rss != np.inf and abs(current_rss - prev_rss) < 0.000001):
+      done = True
+      break
+    prev_b0 = b0
+    prev_b1 = b1
+    b0 = b0 - alpha * rs(y_data, yhat_data)
+    b1 = b1 - alpha * x_times_rs(x_data, y_data, yhat_data)
+    if (overFlowStep > 0 and i0 - overFlowStep >= 5 and timesDivided > 0):
+      alpha *= 10
+      timesDivided -= 1
+      if (timesDivided == 0): overFlowStep = -1
+    elif (i0 % 20 == 0):
+      alpha *= 10
+
+    i0 += 1
+  return (b1, b0)
+
+
+def gdm_log_update_yhat_data(b0, b1s, x_datas):
   yhat_data = [0] * len(x_datas[0])
   for i in range(len(yhat_data)):
-    s = 0
+    s = b0
     for j in range(len(x_datas)):
       s += b1s[j] * x_datas[j][i]
-    yhat_data[i] = s
+    yhat_data[i] = 1 / (1 + np.e ** -(s))
   return yhat_data
 
 # gradient descent algorithm for multiple linear regression,
@@ -164,6 +217,54 @@ def grad_descent_multi(x_datas, y_data):
   overFlowStep = -1
   while(done == False):
     yhat_data = gdm_update_yhat_data(b0, b1s, x_datas)
+    prev_rss = current_rss
+    current_rss = rss(y_data, yhat_data)
+    if (current_rss > prev_rss):
+      timesOverflowed += 1
+      overFlowStep = i0
+      alpha /= 10
+      if (timesOverflowed < 3):
+        timesDivided += 1
+        timesOverflowed = 0
+      b0 = prev_b0
+      b1s = prev_b1s.copy()
+      continue
+    if (prev_rss != np.inf and abs(current_rss - prev_rss) < 0.000001):
+      done = True
+      break
+    prev_b0 = b0
+    prev_b1s = b1s.copy()
+    b0 = b0 - alpha * rs(y_data, yhat_data)
+    for i in range(num_of_predictors):
+      b1s[i] = b1s[i] - alpha * x_times_rs(x_datas[i], y_data, yhat_data)
+    if (overFlowStep > 0 and i0 - overFlowStep >= 5 and timesDivided > 0):
+      alpha *= 10
+      timesDivided -= 1
+      if (timesDivided == 0): overFlowStep = -1
+    elif (i0 % 20 == 0):
+      alpha *= 10
+
+    i0 += 1
+  return (b1s, b0)
+
+# gradient descent algorithm for multinomial
+# log regression, x_datas is a 2D array
+def grad_descent_log_multi(x_datas, y_data):
+  num_of_predictors = len(x_datas)
+  prev_b0 = -1
+  prev_b1s = [-1] * num_of_predictors
+  b0 = 0
+  b1s = [0] * num_of_predictors
+  alpha = 0.000001
+  timesDivided = 0
+  timesOverflowed = 0
+  prev_rss = np.inf
+  current_rss = np.inf
+  done = False
+  i0 = 0
+  overFlowStep = -1
+  while(done == False):
+    yhat_data = gdm_log_update_yhat_data(b0, b1s, x_datas)
     prev_rss = current_rss
     current_rss = rss(y_data, yhat_data)
     if (current_rss > prev_rss):
@@ -243,8 +344,8 @@ def part1():
     yMean = mean(y_data)
     sx = sampleStd(x_data)
     sy = sampleStd(y_data)
-    x_data_mod = [(x - xMean) / sx for x in x_data]
-    y_data_mod = [(y - yMean) / sy for y in y_data]
+    x_data_mod = standardize(x_data)
+    y_data_mod = standardize(y_data)
 
     b1, b0 = grad_descent(x_data_mod, y_data_mod)
     r = b1
@@ -260,17 +361,17 @@ def part1():
   # Print the coefficients for all 12 variables as well
   # as the value of the intercept (ꞵ0) for both versions.
   print("\nPart 1 D:\n")
-  print("When all variables are STANDARDIZED")
+  print("With standardization")
   x_datas = []
   for i in range(1, 13):
     x_data = wines.data[:,i]
     xMean = mean(x_data)
     sx = sampleStd(x_data)
-    x_data_mod = [(x - xMean) / sx for x in x_data]
+    x_data_mod = standardize(x_data)
     x_datas.append(x_data_mod)
   yMean = mean(y_data)
   sy = sampleStd(y_data)
-  y_data_mod = [(y - yMean) / sy for y in y_data]
+  y_data_mod = standardize(y_data)
   b1s, b0 = grad_descent_multi(x_datas, y_data_mod)
   correlation_coeffs = b1s
   for i in range(len(correlation_coeffs)):
@@ -311,28 +412,25 @@ def part2():
   plt.legend()
   plt.show()
 
-  y_data = np.array(is_class1)
+  y_data = is_class1
 
   # B. Relate is_class1 with the other 13 attributes,
   # independently, using standardized logistic
   # regression.  Print the 12 coefficients.
   print("\nPart 2 B:\n")
   for i in range(0, 13):
-    x_data = np.array(wines.data[:,i]).reshape(-1, 1)
-    model = LogisticRegression()
-    model.fit(x_data, y_data)
-    b0 = model.intercept_[0]
-    b1 = model.coef_[0][0]
+    x_data = standardize(wines.data[:,i])
+    b1, b0 = grad_descent_log(x_data, y_data)
     print("coefficient " + str(i + 1) + " is " + str(b1))
 
-   # x = np.linspace(min(x_data) - 1, max(x_data) + 1, 50)
-   # plt.scatter(x_data, y_data, c='orange', label='data', alpha=0.5)
-   # plt.plot(x, (np.e ** (b0 + b1 * x)) / (1 + np.e **(b0 + b1 * x)))
-   # plt.xlabel("x_data")
-   # plt.ylabel("is_class1")
-   # plt.title("x effect on is_class1")
-   # plt.legend()
-   # plt.show()
+    #x = np.linspace(min(x_data) - 1, max(x_data) + 1, 50)
+    #plt.scatter(x_data, y_data, c='orange', label='data', alpha=0.5)
+    #plt.plot(x, (np.e ** (b0 + b1 * x)) / (1 + np.e **(b0 + b1 * x)))
+    #plt.xlabel("x_data")
+    #plt.ylabel("is_class1")
+    #plt.title("x effect on is_class1")
+    #plt.legend()
+    #plt.show()
 
 
   # C. Relate all 13 attributes to is_class1 at once
@@ -346,37 +444,30 @@ def part2():
   # coefficients for all 12 variables as well as
   # the value of the intercept (ꞵ0) for both versions.
   print("\nPart 2 C:\n")
-  x_data = np.array([])
-  n = len(wines.data[:,0])
+  print("With standardization")
+  x_datas = []
   for i in range(0, 13):
-    data = StandardScaler().fit_transform(np.reshape(np.array(wines.data[:,i]), (-1, 1)))
-    data = np.reshape(data, (1, -1))
-    x_data = np.append(x_data, data)
-  x_data = np.transpose(np.reshape(x_data, (13, n)))
+    x_data = wines.data[:,i]
+    x_data_mod = standardize(x_data)
+    x_datas.append(x_data_mod)
 
-  # with standardization
-  print("WITH STANDARDIZATION:")
-  model = LogisticRegression(multi_class='multinomial', max_iter=10000, solver='saga')
-  model.fit(x_data, y_data)
-  b0 = model.intercept_[0]
-  b1 = model.coef_[0]
-  print("Model intercept: ", b0)
-  print("Model coefficients: ", b1)
+  b1s, b0 = grad_descent_log_multi(x_datas, y_data)
+  correlation_coeffs = b1s
+  for i in range(len(correlation_coeffs)):
+    print("Correlation coefficient r" + str(i + 1) + " is", correlation_coeffs[i])
+  print("Y-intercept is", b0)
 
-  # without standardization
-  print("WITHOUT STANDARDIZATION:")
-  x_data = np.array([])
-  n = len(wines.data[:,0])
+  print("\nWithout standardization")
+  x_datas = []
   for i in range(0, 13):
-    x_data = np.append(x_data, np.array(wines.data[:,i]))
-  x_data = np.transpose(np.reshape(x_data, (13, n)))
+    x_data = wines.data[:,i]
+    x_datas.append(x_data)
 
-  model = LogisticRegression(multi_class='multinomial', max_iter=10000, solver='saga')
-  model.fit(x_data, y_data)
-  b0 = model.intercept_[0]
-  b1 = model.coef_[0]
-  print("Model intercept: ", b0)
-  print("Model coefficients: ", b1)
+  b1s, b0 = grad_descent_log_multi(x_datas, y_data)
+  correlation_coeffs = b1s
+  for i in range(len(correlation_coeffs)):
+    print("Correlation coefficient r" + str(i + 1) + " is", correlation_coeffs[i])
+  print("Y-intercept is", b0)
 
   return
 
